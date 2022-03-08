65b16a9d4712bedc9f515660aaead74e29ac393f
MD <56653847+snowthesprite@users.noreply.github.com>
2022-03-08 22:58:06 +0000

    Update

--- tic_tac_toe/games/game_tree_2.py
+++ tic_tac_toe/games/game_tree_2.py
@@ -4,7 +4,7 @@ class Node :
         self.children = []
         self.player = player
         self.parent = parent
-        #self.score = None
+        self.score = None
         self.winner = self.check_for_winner()
     
     def check_for_winner(self) :
@@ -36,6 +36,7 @@ class TicTacToeTree :
         self.total_nodes = 1
         self.leaf_nodes = 0 
         self.build_game_tree() 
+        self.check_scores()
 
     def create_children(self, parent) :
         if parent.winner != None :
@@ -46,51 +47,82 @@ class TicTacToeTree :
             game_state = [row.copy() for row in parent.state]
             game_state[row_id][col_id] = self.plr_marks[parent.player]
             child = Node(parent, next_plr, game_state)
-            self.check_for_winner(child)
             parent.children.append(child)
             self.total_nodes += 1
 
     def build_game_tree(self) :
-        #parent_leaf_nodes = []
+        leaf_nodes = []
         queue = [self.root]
         while queue != [] :
             self.create_children(queue[0])
             queue.extend(queue[0].children)
-            #if queue[0].children == [] :
-                #parent_leaf_nodes.append(queue[0].parent)
+            if queue[0].children == [] :
+                leaf_nodes.append(queue[0])
             queue.pop(0)
-        #self.assign_values(parent_leaf_nodes)
+        self.assign_values(leaf_nodes)
 
-    def assign_values(self, parent_leaf_nodes) :
-        unassigned = parent_leaf_nodes
-        while unassigned != [] :
-            print(unassigned[0].state)
-            print(len(unassigned))
-            i = 0
-            total_removed = 0
-            removed = 0
-            for node in unassigned.copy() :
-                if i % 10000 == 0 :
-                    print('iteration:',i)
-                    print('actually removed:',removed)
-                    print('removed:',total_removed)
-                    print()
-                i+=1
-                child_scores = [child.score for child in node.children]
-                if None in child_scores :
-                    continue
-                if node.player == self.max_plr :
-                    node.score = max(child_scores)
+    def assign_values(self, leaf_nodes) :
+        unassigned = leaf_nodes
+        print(len(unassigned))
+        index = 0
+        total_removed = 0
+        removed = 0
+        i =0
+        while index <= len(unassigned) :
+            node = unassigned[index]
+            if i % 10000 == 0 :
+                print('iteration:',i, 'index',index)
+                print('actually removed:',removed)
+                print('removed:',total_removed)
+                print(len(unassigned))
+                print()
+            index+=1
+            i+=1
+            child_scores = [child.score for child in node.children]
+            if None in child_scores :
+                continue
+            elif child_scores == [] :
+                if node.winner == 'Tie' :
+                    node.score = 0
+                elif node.winner == self.max_plr :
+                    node.score = 1
                 else :
-                    node.score = min(child_scores)
-                unassigned.remove(node)
-                removed+=1
-                total_removed +=1 
-                if node.parent not in unassigned :
-                    unassigned.append(node.parent)
-                    removed-= 1
-            
-## yeah I lifted these from the shared game file what of it
+                    node.score = -1
+            elif node.player == self.max_plr :
+                node.score = max(child_scores)
+            else :
+                node.score = min(child_scores)
+
+            unassigned.remove(node)
+            index -= 1
+            removed+=1
+            total_removed +=1 
+            if node.parent not in unassigned :
+                unassigned.append(node.parent)
+                removed-= 1
+            if index == len(unassigned) and len(unassigned) > 1 :
+                index = 0
+
+    def check_scores(self) :
+        self.root.score == 'root'
+        queue = [self.root]
+        while queue != [] :
+            if queue[0].score == None :
+                print('This node has no score')
+                print(queue[0].state)
+                print('This nodes children are')
+                for node in queue[0].children :
+                    print(node.state)
+                    print(node.score)
+                    print()
+                print('this nodes parent is')
+                print(queue[0].parent.state)
+                print(queue[0].parent.score)
+                print('\n\n\n')
+            queue.extend(queue[0].children)
+            queue.pop(0)
+   
+    ## yeah I lifted these from the shared game file what of it 
 
     def get_possible_moves(self, board) :
         possible_moves = [(i,j) for i in range(3) for j in range(3) if board[i][j] == None]
