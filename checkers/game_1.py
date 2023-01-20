6b1f08a6ee5d92c93b702b0aa7f80db6b3a9d72a
NamelessEarth <mpapaya9@gmail.com>
2023-01-20 21:39:18 +0000

    finished movement functs, working on game sequence

--- checkers/game_1.py
+++ checkers/game_1.py
@@ -1,13 +1,13 @@
-#min max player
+#work on making board dict (possibly)
 
 from random import random
 
 class Con4 :
     def __init__(self, players, loop):
-        self.rand = loop#round(random())
+        self.rand = loop
         self.players = players
-        self.board = [''.join(['0' for _ in range(7)]) for __ in range(6)]
-        self.determine_player_order()
+        self.board = [[(i+j)%2 * ((3 - (j<3)-(j<4))%3) for i in range(8)] for j in range(8)]
+        #self.determine_player_order()
         self.set_player_numbers()
         self.round = 1
         self.winner = None
@@ -20,16 +20,68 @@ class Con4 :
         if self.rand % 2 == 1:
             self.players = self.players[::-1]
     
-    def get_possible_moves_col(self) :
-        possible_moves = []
-        
-        cols = [[(row,col) for row in range(5,-1,-1)] for col in range(7)]
-        for colm in cols :
-            for (row, col) in colm :
-                if self.board[row][col] == '0' :
-                    possible_moves.append(col)
-                    break
-        return possible_moves
+    def get_possible_trans(self, piece) :
+        if piece < 0 :
+            return [(1,1), (1,-1), (-1,1), (-1,-1)]
+        elif piece == 1 :
+            return [(-1,1), (-1,-1)]
+        elif piece == 2 :
+            return [(1,1), (1,-1)]
+    
+    def out_of_bounds(self, coord) :
+        if coord[1] > 7 or coord[1] < 0 or coord[0] > 7 or coord[0] < 0 :
+            return True
+        return False
+
+    def friend_foe(self, coord, old_coord) :
+        cur = self.board[old_coord[0]][old_coord[1]]
+        unkn = self.board[coord[0]][coord[1]]
+        if abs(cur) != abs(unkn) :
+            return True
+        return False
+
+    def next_clear(self, coord, trans) :
+        new_coord = coord
+        if self.board[new_coord[0]][new_coord[1]] == 0 and not self.out_of_bounds(new_coord) :
+            return True
+
+    def get_possible_moves(self, coord) :
+        translation = self.get_possible_trans(self.board[coord[0]][coord[1]])
+        combat = []
+        non = []
+        for trans in translation :
+            new_coord = [coord[0] + trans[0], coord[1] + trans[1]]
+            if this.out_of_bounds(new_coord) :
+                continue
+            if this.friend_foe(new_coord, coord) and this.next_clear(new_coord, trans) :
+                combat.append(trans)
+            else :
+                continue
+            non.append(trans)
+        if combat != [] :
+            return (combat, True)
+        return (non, False)
+
+    def get_pieces(self, plr_num) :
+        pieces = []
+        for row in range(8) :
+            for col in range(8) :
+                if abs(self.board[row][col]) == plr_num :
+                    pieces.append((row, col))
+
+    def get_all_moves(self, plr_num) :
+        pieces = self.get_pieces(plr_num)
+        combat = []
+        non = []
+        atk = False
+        for coord in pieces :
+            translations = self.get_possible_moves(coord)
+            if translations[1] == True :
+                atk = True
+                combat.extend([(coord, trans) for trans in translations[0]])
+            if not atk :
+                non.extend([(coord, trans) for trans in translations[0]])
+
 
     def complete_round(self):
         for player in self.players:
@@ -52,37 +104,7 @@ class Con4 :
         #'''
 
     def check_for_winner(self) :
-        rows = [self.board[row] for row in range(6)] #row
-        cols = [''.join([self.board[row][col] for row in range(6)]) for col in range(7)]
-        l_dias = []
-        r_dias = []
-        diagonals = [(3,0),(4,0),(5,0),(5,1),(5,2),(5,3)]
-        for (row, col) in diagonals :
-            i=0
-            l_dia = []
-            r_dia = []
-            while row-i >=0 and col+i <= 6 :
-                l_dia.append(self.board[row-i][col+i])
-                r_dia.append(self.board[row-i][6-col-i])
-                i+= 1
-            l_dias.append(''.join(l_dia))
-            r_dias.append(''.join(r_dia))
-        
-        thing = rows + cols + l_dias + r_dias
-
-        tie = []
-
-        for stuff in thing :
-            tie.append('0' in stuff)
-            if stuff.count('0') > len(stuff) - 4 :
-                continue
-            if '1111' in stuff :
-                return 1
-            elif '2222' in stuff :
-                return 2
-
-        if True not in tie:
-            return 'Tie'
+        pass
 
     def print_board(self):
         for row in self.board:
