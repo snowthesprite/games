e697652257d52e449bfa508c4ab42379732eca73
NamelessEarth <mpapaya9@gmail.com>
2023-02-07 20:54:16 +0000

    Forgot to commit before, no more manditory capture

--- checkers/game_1.py
+++ checkers/game_1.py
@@ -4,7 +4,7 @@ from random import random
 
 class Con4 :
     def __init__(self, players, loop):
-        self.rand = loop
+        self.rand = loop#round(random())
         self.players = players
         self.board = [[(i+j)%2 * ((3 - (j<3)-(j<4))%3) for i in range(8)] for j in range(8)]
         #self.determine_player_order()
@@ -12,13 +12,16 @@ class Con4 :
         self.round = 1
         self.winner = None
 
+
     def set_player_numbers(self): 
         self.players[0].set_player_number(1)
         self.players[1].set_player_number(2)
+
     
     def determine_player_order(self):
         if self.rand % 2 == 1:
             self.players = self.players[::-1]
+
     
     def get_possible_trans(self, piece) :
         if piece < 0 :
@@ -27,40 +30,63 @@ class Con4 :
             return [(-1,1), (-1,-1)]
         elif piece == 2 :
             return [(1,1), (1,-1)]
+
     
     def out_of_bounds(self, coord) :
         if coord[1] > 7 or coord[1] < 0 or coord[0] > 7 or coord[0] < 0 :
             return True
         return False
 
-    def friend_foe(self, coord, old_coord) :
+
+    def foe_present(self, coord, old_coord) :
         cur = self.board[old_coord[0]][old_coord[1]]
         unkn = self.board[coord[0]][coord[1]]
-        if abs(cur) != abs(unkn) :
+        if abs(cur) != abs(unkn) and unkn != 0 :
             return True
         return False
+
+
+    def friend_present(self, coord, old_coord) :
+        cur = self.board[old_coord[0]][old_coord[1]]
+        unkn = self.board[coord[0]][coord[1]]
+        if abs(cur) == abs(unkn) and unkn != 0 :
+            return True
+        return False
+
 
     def next_clear(self, coord, trans) :
         new_coord = coord
         if self.board[new_coord[0]][new_coord[1]] == 0 and not self.out_of_bounds(new_coord) :
             return True
 
+
     def get_possible_moves(self, coord) :
         translation = self.get_possible_trans(self.board[coord[0]][coord[1]])
-        combat = []
-        non = []
+        can_move = []
         for trans in translation :
             new_coord = [coord[0] + trans[0], coord[1] + trans[1]]
-            if this.out_of_bounds(new_coord) :
+            if self.out_of_bounds(new_coord) or self.friend_present(new_coord, coord) :
                 continue
-            if this.friend_foe(new_coord, coord) and this.next_clear(new_coord, trans) :
-                combat.append(trans)
+            if self.foe_present(new_coord, coord) and self.next_clear(new_coord, trans) :
+                can_move.append((2*trans[0], 2*trans[1]))
             else :
+                can_move.append(trans)
+        return can_move
+
+
+    def get_pos_moves_combo(self, coord) :
+        translation = self.get_possible_trans(self.board[coord[0]][coord[1]])
+        can_move = []
+        for trans in translation :
+            new_coord = [coord[0] + trans[0], coord[1] + trans[1]]
+            if this.out_of_bounds(new_coord) or this.board[coord[0]][coord[1]] != 0 :
                 continue
-            non.append(trans)
-        if combat != [] :
-            return (combat, True)
-        return (non, False)
+            if this.foe_present(new_coord, coord) and this.next_clear(new_coord, trans) :
+                can_move.append((2*trans[0], 2*trans[1]))
+            else :
+                can_move.append(trans)
+        return can_move
+
 
     def get_pieces(self, plr_num) :
         pieces = []
@@ -69,18 +95,43 @@ class Con4 :
                 if abs(self.board[row][col]) == plr_num :
                     pieces.append((row, col))
 
+
     def get_all_moves(self, plr_num) :
         pieces = self.get_pieces(plr_num)
-        combat = []
-        non = []
-        atk = False
+        can_move = []
         for coord in pieces :
             translations = self.get_possible_moves(coord)
-            if translations[1] == True :
-                atk = True
-                combat.extend([(coord, trans) for trans in translations[0]])
-            if not atk :
-                non.extend([(coord, trans) for trans in translations[0]])
+            can_move.extend([(coord, trans) for trans in translations])
+        return can_move
+
+
+    def capture(self, move) :
+        coord_2 = move[0]
+        coord = (coord_2[0] + move[1][0], coord_2[1] + move[1][1])
+        self.update_board(coord_2, coord)
+        coord_2 = (coord[0] + move[1][0], coord[1] + move[1][1])
+        self.update_board(coord, coord_2)
+
+
+    def side_moves(self, move) :
+        new_coord = (move[0][0] + move[1][0], move[0][1] + move[1][1])
+        while 2 in move[1] or -2 in move[1] :
+            self.capture(move)
+            new_coord = (new_coord[0] + move[1][0], new_coord[1] + move[1][1])
+            pos_moves = self.get_pos_moves_combo(new_coord)
+            if pos_moves[1] :
+                move = self.player[plr_num-1].choose_move([(new_coord, trans) for trans in pos_moves[0]])
+            else :
+                break
+
+
+    def complete_round(self) :
+        plr_num = 1
+        while self.winner == None :
+            move = self.players[plr_num-1].choose_move(self.get_all_moves(plr_num))
+            self.side_moves(move)
+
+
 
 
     def complete_round(self):
@@ -121,10 +172,7 @@ class Con4 :
             
         #print('\n')
     
-    def update_board(self, player, choice) : 
-        #print(choice)
-        for row in range(5,-1,-1) :
-            if self.board[row][choice] == '0' :
-                choice = (row, choice)
-                break
-        self.board[choice[0]] = self.board[choice[0]][:choice[1]] + str(player.num) + self.board[choice[0]][choice[1]+1:]
+    def update_board(self, old_coord, new_coord) : 
+        piece = self.board[old_coord[0]][old_coord[1]]
+        self.board[old_coord[0]][old_coord[1]] = 0
+        self.board[new_coord[0]][new_coord[1]] = piece
